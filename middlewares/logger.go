d6130bff055ad1c4a0b10aedd5a7a216bb964da3
etwodev <ewoods@adaptavist.com>
2024-06-20 22:50:58 +0100

    chore: update ip logging

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -1,8 +1,10 @@
 package middlewares
 
 import (
+	"net"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -24,7 +26,7 @@ func Logger() func(next http.Handler) http.Handler {
 			duration := time.Since(start)
 
 			log.Info().
-				Str("IP", r.RemoteAddr).
+				Str("IP", getIPAddress(r)).
 				Str("Url", r.URL.Path).
 				Str("Method", r.Method).
 				Dur("response_time", duration).
@@ -32,4 +34,24 @@ func Logger() func(next http.Handler) http.Handler {
 		}
 		return http.HandlerFunc(fn)
 	}
+}
+
+func getIPAddress(r *http.Request) string {
+	// Try to get the IP from the X-Forwarded-For header
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	if xForwardedFor != "" {
+		// This header can contain multiple IPs. The first one is the original client IP
+		parts := strings.Split(xForwardedFor, ",")
+		return strings.TrimSpace(parts[0])
+	}
+
+	// If there is no X-Forwarded-For header, use the RemoteAddr from the request
+	// This might include a port, so we split it off if present
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		// If there was an error, it might be because there's no port in the RemoteAddr
+		// In that case, just return the RemoteAddr as the IP
+		return r.RemoteAddr
+	}
+	return ip
 }
